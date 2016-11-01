83939767cb2de51cec2730e463f0aa2b0a4c1608
Raghu Betina <raghu@firstdraft.com>
2016-11-01 14:37:34 +0000

    Generate names resource with starter:resource

---
+++ app/controllers/names_controller.rb
@@ -0,0 +1,46 @@
+class NamesController < ApplicationController
+  def index
+    @names = Name.all
+  end
+
+  def show
+    @name = Name.find(params[:id])
+  end
+
+  def new
+    @name = Name.new
+  end
+
+  def create
+    @name = Name.new
+
+    if @name.save
+      redirect_to "/names", :notice => "Name created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @name = Name.find(params[:id])
+  end
+
+  def update
+    @name = Name.find(params[:id])
+
+
+    if @name.save
+      redirect_to "/names", :notice => "Name updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @name = Name.find(params[:id])
+
+    @name.destroy
+
+    redirect_to "/names", :notice => "Name deleted."
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Name resource:
+  # CREATE
+  get "/names/new", :controller => "names", :action => "new"
+  post "/create_name", :controller => "names", :action => "create"
+
+  # READ
+  get "/names", :controller => "names", :action => "index"
+  get "/names/:id", :controller => "names", :action => "show"
+
+  # UPDATE
+  get "/names/:id/edit", :controller => "names", :action => "edit"
+  post "/update_name/:id", :controller => "names", :action => "update"
+
+  # DELETE
+  get "/delete_name/:id", :controller => "names", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
